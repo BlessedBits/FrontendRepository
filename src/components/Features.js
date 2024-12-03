57177d2d71f6612be1746cc82a00416cc60b6d27
oleksandr-ilnytskyi <146804152+oleksandr-ilnytskyi@users.noreply.github.com>
2024-12-03 17:34:38 +0200

    Add home page components

---
+++ src/components/Features.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Features = () => {
+    return (
+        <section className="features">
+            <h2>Можливості платформи</h2>
+            <div className="for-teachers">
+                <h3>Для вчителів:</h3>
+                <a href="#">Журнал</a>
+                <a href="#">Дистанційне навчання</a>
+                <a href="#">Онлайн уроки</a>
+                <a href="#">Lorem ipsum</a>
+            </div>
+            <div className="for-students">
+                <h3>Для учнів:</h3>
+                <a href="#">Щоденник</a>
+                <a href="#">Друзі</a>
+                <a href="#">Розклад уроків</a>
+                <a href="#">Онлайн підручники</a>
+            </div>
+        </section>
+    );
+};
+
+export default Features;
---
+++ src/components/Intro.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Intro = () => {
+    return (
+        <section className="intro" id="intro">
+            <h1>Подаруймо дітям знання та майбутнє</h1>
+            <p>
+                <strong>SchoolHub</strong> пропонує простий і доступний формат онлайн-навчання,
+                який відповідає потребам сучасної, української школи.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Donec ullamcorper felis sed elit condimentum, quis suscipit mauris convallis.
+                Cras scelerisque urna vel tortor porta malesuada. Proin urna urna, aliquam nec purus quis,
+                condimentum facilisis dolor. Mauris eget dignissim eros. Aenean risus tortor, elementum sit
+                amet faucibus nec, rutrum et dui. Quisque vulputate eros id nunc consequat sagittis.
+                Suspendisse id egestas libero. Nam ac augue quam. Aenean vitae libero at urna tincidunt interdum.
+                Nullam venenatis venenatis ex, eu consequat leo mattis ac.
+            </p>
+        </section>
+    );
+};
+
+export default Intro;
