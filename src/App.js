9e17c986bba7fd1efc474cd1a8781061cf96ce71
OlehRoman <oleh.roman.shi.2023@lpnu.ua>
2025-01-04 00:04:18 +0200

    Add profile page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SchoolPage from "./pages/SchoolPage";
 import HomePage from "./pages/homepage";
-
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
     const [isAuthModalOpen, setAuthModalOpen] = useState(false);
@@ -13,12 +13,12 @@ function App() {
         <Router>
             <Routes>
                 {/* Головна сторінка */}
-                <Route
-                    path="/"
-                    element={ <HomePage/>}
-                />
+                <Route path="/" element={<HomePage />} />
                 {/* Сторінка "Школа" */}
-                <Route path="school/:schoolId" element={<SchoolPage/> } /> 
+                <Route path="school/:schoolId" element={<SchoolPage />} />
+                {/* Сторінка профілю */}
+                <Route path="my-profile/:userId" element={<ProfilePage />} />
+
             </Routes>
         </Router>
     );
