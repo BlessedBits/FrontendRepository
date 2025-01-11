0ca914cf5e30fbb3b501c6cf14850a0b77132ff9
oleksandr-ilnytskyi <146804152+oleksandr-ilnytskyi@users.noreply.github.com>
2025-01-11 13:17:14 +0200

    Changed SchoolApi to work with cloud server

--- src/components/misc/SchoolApi.js
+++ src/components/misc/SchoolApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = 'https://ypgblessedbits.azurewebsites.net/schools';
-const BASE_URL = 'http://localhost:8080/schools';
+const BASE_URL = 'https://ypgblessedbits.azurewebsites.net/schools';
+// const BASE_URL = 'http://localhost:8080/schools';
 
 const instance = axios.create({
     baseURL: BASE_URL,
