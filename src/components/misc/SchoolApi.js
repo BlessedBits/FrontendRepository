426924ad7c30fd190321b81a404ddc5c809efc60
oleksandr-ilnytskyi <146804152+oleksandr-ilnytskyi@users.noreply.github.com>
2025-01-11 02:54:54 +0200

    Added auth interaction with jwt accessToken

---
+++ src/components/misc/SchoolApi.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+// const BASE_URL = 'https://ypgblessedbits.azurewebsites.net/schools';
+const BASE_URL = 'http://localhost:8080/schools';
+
+const instance = axios.create({
+    baseURL: BASE_URL,
+})
+
+instance.interceptors.request.use(config => {
+    const accessToken = localStorage.getItem("accessToken");
+    const tokenType = localStorage.getItem("tokenType") || 'Bearer ';
+
+    if (accessToken) {
+        config.headers.Authorization = `${tokenType}${accessToken}`;
+    }
+
+    return config;
+}, error => {
+    return Promise.reject(error);
+})
+
+export const getAllSchools = async () => {
+    const response = await instance.get('/');
+    if (response && response.status === 200) {
+        return response.data;
+    }
+}
