8d1ef72cb2d25546b1a7bdfc3a6402d2695d9957
oleksandr-ilnytskyi <146804152+oleksandr-ilnytskyi@users.noreply.github.com>
2025-01-25 01:54:15 +0200

    Removed redundant lines in auth.js api

--- src/api/auth.js
+++ src/api/auth.js
@@ -9,8 +9,6 @@ export const login = async (username, password, rememberMe, setAuth) => {
                 withCredentials: true,
             });
         const {accessToken, tokenType} = response?.data;
-        localStorage.setItem('accessToken', accessToken);
-        localStorage.setItem('tokenType', tokenType);
         setAuth({accessToken, tokenType});
     } catch (err) {
         if (!err?.response) {
@@ -28,32 +26,3 @@ export const register = async (username, password, email = null) => {
     }
     return response.data;
 }
-//
-// let isScheduled = false;
-// export const scheduleTokenRefresh = () => {
-//     if (isScheduled) {
-//         console.log("Already scheduled");
-//         return;
-//     }
-//
-//     const accessToken = localStorage.getItem('accessToken');
-//     if (!accessToken) return;
-//     const checkFrequency = 0.25 * 60 * 1000;
-//
-//     setTimeout(async () => {
-//         try {
-//             const response = await instance.get(`/token/refresh`, {});
-//             const newAccessToken = response.data.accessToken;
-//             if (response.status === 200) {
-//                 localStorage.setItem('accessToken', newAccessToken);
-//                 scheduleTokenRefresh();
-//                 isScheduled = true;
-//                 console.log("Scheduled");
-//             }
-//         } catch (error) {
-//             console.error("Failed to refresh access token:", error);
-//             // localStorage.removeItem('accessToken');
-//             // window.location.href = '/';
-//         }
-//     }, checkFrequency);
-// }
