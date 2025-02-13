a09fe1d6907c24490b4400b49287f0a8b1c12b77
kovalets-vlad <vladyslav.kovalets.shi.2023@gmail.com>
2025-02-13 11:55:07 +0200

    Finnaly update school page

--- src/api/course.js
+++ src/api/course.js
@@ -1,23 +1,15 @@
 import { executeRequest } from "../utils/apiUtils";
 
-export const getUserCourses = async (
-    userId,
-    userRole,
-    axiosPrivateInstance
-) => {
+export const getUserCourses = async (userId, userRole, axiosPrivateInstance) => {
     try {
-        const data = await executeRequest(
-            () => axiosPrivateInstance.get(`users/${userId}`),
-            200
-        );
+        const data = await executeRequest(() => axiosPrivateInstance.get(`users/${userId}`), 200);
 
         if (!data) {
             throw new Error("Не вдалося отримати дані користувача.");
         }
-        const url =
-            userRole === "SCHOOL_ADMIN"
-                ? `schools/${data.schoolId}/courses?include=modules`
-                : `classes/${data.userClassId}/courses?include=modules`;
+        const url = ["TEACHER", "SCHOOL_ADMIN"].includes(userRole)
+            ? `schools/${data.schoolId}/courses?include=modules`
+            : `classes/${data.userClassId}/courses?include=modules`;
 
         return await executeRequest(() => axiosPrivateInstance.get(url), 200);
     } catch (err) {
@@ -27,34 +19,18 @@ export const getUserCourses = async (
 };
 
 export const getModules = async (id, axiosPrivateInstance) => {
-    return executeRequest(
-        () => axiosPrivateInstance.get(`/courses/${id}/modules`),
-        200,
-        "Module found"
-    );
+    return executeRequest(() => axiosPrivateInstance.get(`/courses/${id}/modules`), 200, "Module found");
 };
 
 export const getMaterials = async (id, axiosPrivateInstance) => {
-    return executeRequest(
-        () => axiosPrivateInstance.get(`modules/${id}/materials`),
-        200,
-        "Material found"
-    );
+    return executeRequest(() => axiosPrivateInstance.get(`modules/${id}/materials`), 200, "Material found");
 };
 
 export const getaAssignments = async (id, axiosPrivateInstance) => {
-    return executeRequest(
-        () => axiosPrivateInstance.get(`/modules/${id}/assignments`),
-        200,
-        "Assignments found"
-    );
+    return executeRequest(() => axiosPrivateInstance.get(`/modules/${id}/assignments`), 200, "Assignments found");
 };
 
-export const createCourse = async (
-    teacherId,
-    courseName,
-    axiosPrivateInstance
-) => {
+export const createCourse = async (teacherId, courseName, axiosPrivateInstance) => {
     if (!courseName || !teacherId) {
         throw new Error("Назва курсу та ID викладача є обов'язковими");
     }
@@ -72,29 +48,15 @@ export const createCourse = async (
     );
 };
 
-export const createModule = async (
-    courseId,
-    moduleName,
-    axiosPrivateInstance
-) => {
+export const createModule = async (courseId, moduleName, axiosPrivateInstance) => {
     return executeRequest(
-        () =>
-            axiosPrivateInstance.post(
-                "/courses/modules",
-                { name: moduleName },
-                { params: { courseId } }
-            ),
+        () => axiosPrivateInstance.post("/courses/modules", { name: moduleName }, { params: { courseId } }),
         201,
         "Module created"
     );
 };
 
-export const createMaterial = async (
-    courseId,
-    moduleId,
-    material,
-    axiosPrivateInstance
-) => {
+export const createMaterial = async (courseId, moduleId, material, axiosPrivateInstance) => {
     return executeRequest(
         () =>
             axiosPrivateInstance.post("/courses/modules/materials", material, {
@@ -105,31 +67,15 @@ export const createMaterial = async (
     );
 };
 
-export const createAssignment = async (
-    courseId,
-    moduleId,
-    assignment,
-    axiosPrivateInstance
-) => {
+export const createAssignment = async (courseId, moduleId, assignment, axiosPrivateInstance) => {
     return executeRequest(
-        () =>
-            axiosPrivateInstance.post(
-                "/courses/modules/assignments",
-                assignment,
-                { params: { courseId, moduleId } }
-            ),
+        () => axiosPrivateInstance.post("/courses/modules/assignments", assignment, { params: { courseId, moduleId } }),
         201,
         "Assignment created"
     );
 };
 
-export const createSubmission = async (
-    courseId,
-    moduleId,
-    assignmentId,
-    submissionUrl,
-    axiosPrivateInstance
-) => {
+export const createSubmission = async (courseId, moduleId, assignmentId, submissionUrl, axiosPrivateInstance) => {
     return executeRequest(
         () =>
             axiosPrivateInstance.post(
@@ -142,17 +88,9 @@ export const createSubmission = async (
     );
 };
 
-export const gradeSubmission = async (
-    submissionId,
-    grade,
-    axiosPrivateInstance
-) => {
+export const gradeSubmission = async (submissionId, grade, axiosPrivateInstance) => {
     return executeRequest(
-        () =>
-            axiosPrivateInstance.post(
-                "/courses/modules/assignments/submissions/grade",
-                { submissionId, grade }
-            ),
+        () => axiosPrivateInstance.post("/courses/modules/assignments/submissions/grade", { submissionId, grade }),
         201,
         "Submission graded"
     );
